f1930609ca6422cc3f34fc84e37b03777b1c1f09
Siddhi <91131867+siddhi-gh@users.noreply.github.com>
2023-06-18 00:23:32 +0530

    COMMIT3: Add abut me and contact section

---
+++ src/components/aboutMe/AboutMe.jsx
@@ -0,0 +1,54 @@
+import {
+  AboutContent,
+  ContentImage,
+  ContentText,
+  ImageSideContainer,
+  SectionContainer,
+  SectionWrapper,
+  WorkEmoji,
+} from "./AboutMe.styles";
+
+function AboutMe() {
+  return (
+    <>
+      <SectionWrapper>
+        <SectionContainer>
+          <AboutContent>
+            <ImageSideContainer>
+              <WorkEmoji
+                src="\u{1F469}\u{1F3FB}\u{200D}\u{1F4BB}"
+                alt="emoji"
+              />
+              <ContentImage
+                src="https://stefantopalovicdev.vercel.app/static/media/about-img.62b47e7f183d4b4e9feb.webp"
+                alt="mee"
+              />
+              <span>
+                <img src={"RoundedText"} alt="text" />
+              </span>
+            </ImageSideContainer>
+            <ContentText>
+              <h3>About me</h3>
+              <h4>
+                A dedicated Front-end Developer <br /> based in Bangalore, India
+                📍
+              </h4>
+              <p>
+                As a Junior Front-End Developer, I possess an impressive arsenal
+                of skills in CSS, JavaScript, TypeScript and React. I excel in
+                designing and maintaining responsive websites that offer a
+                smooth user experience. My expertise lies in crafting dynamic,
+                engaging interfaces through writing clean and optimized code and
+                utilizing cutting-edge development tools and techniques. I am
+                also a team player who thrives in collaborating with
+                cross-functional teams to produce outstanding web applications.
+              </p>
+            </ContentText>
+          </AboutContent>
+        </SectionContainer>
+      </SectionWrapper>
+    </>
+  );
+}
+
+export default AboutMe;
---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,39 @@
+export const Contact = () => {
+  return (
+    <>
+      <section id="contact" className="contact">
+        <div className="container">
+          <div className="contact__content">
+            <div className="contact__title">
+              <p>contact</p>
+              <h3>Don't be shy! Hit me up! 👇</h3>
+            </div>
+            <div className="contact__icons">
+              <div className="contact__icon-box">
+                <span>
+                  <i className="fa-solid fa-map-location-dot"></i>
+                </span>
+                <div className="contact__info">
+                  <h3>Location</h3>
+                  <p>Bangalore, India</p>
+                </div>
+              </div>
+
+              <div className="contact__icon-box">
+                <span>
+                  <i className="fa-solid fa-envelope-open-text"></i>
+                </span>
+                <div className="contact__info">
+                  <h3>Mail</h3>
+                  <a href="mailto:siddhiusgupta@gmail.com">
+                    siddhiusgupta@gmail.com
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
