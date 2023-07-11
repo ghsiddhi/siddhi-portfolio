887433b6f214ad4eb7b51ce2d8db8e0a1a9654dc
siddhi-gh <siddhiusgupta@gmail.com>
2023-07-11 22:12:27 +0530

    FIX: update github link in Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -31,7 +31,7 @@ export const Footer = () => {
               aria-label="github"
               target="_blank"
               rel="noreferrer"
-              href="https://github.com/siddhijll"
+              href="https://github.com/siddhi-gh"
             >
               <FooterRcSocialIcon>
                 <FaGithub />
