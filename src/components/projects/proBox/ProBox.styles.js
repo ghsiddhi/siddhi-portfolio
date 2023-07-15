9ad99e1434219f11eec1396880157ae010260a6a
siddhi-gh <siddhiusgupta@gmail.com>
2023-07-15 11:52:47 +0530

    FEAT: add ProBox styles

---
+++ src/components/projects/proBox/ProBox.styles.js
@@ -0,0 +1,12 @@
+import { styled } from "@mui/material";
+
+export const ProBox = styled("div")(() => ({
+  backgroundColor: "#fff",
+  borderRadius: "1.7rem",
+  boxShadow: "0 0 10px rgba(0, 0, 0, .1)",
+  display: "flex",
+  flexDirection: "row",
+  gap: "8rem",
+  height: "40rem",
+  padding: "2rem",
+}));
