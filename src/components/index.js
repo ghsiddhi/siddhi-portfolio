1bae36e82148951f5f33a41d131879a60c9eddca
siddhi-gh <siddhiusgupta@gmail.com>
2023-07-16 13:53:46 +0530

    REFACTOR: add export in components folder

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export * from './aboutMe/AboutMe';
+export * from './contact/Contact';
+export * from './footer/Footer';
+export * from './hero/Hero';
+export * from './navbar/Navbar';
+export * from './projects/Projects';
