27bd67b1c7174e086848ecb9c55dbc82ea542d19
siddhi-gh <siddhiusgupta@gmail.com>
2023-08-12 23:34:20 +0530

    CHORE: remove images folder

--- src/images/index.js
+++
@@ -1,4 +0,0 @@
-export { default as AboutImg} from './about-img.webp';
-export { default as RoundedText} from './text2.svg';
-export { default as WomanTechnologist} from './womanTechnologist.png';
-export { default as DeskSetup} from './desk-setup.png';
