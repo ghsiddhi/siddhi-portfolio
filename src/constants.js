44f290daf300bd8a97bfe0946b36826071b34c72
siddhi-gh <Siddhi.Gupta@ap.jll.com>
2023-12-09 12:18:59 +0530

    REFACTOR: create constants file

---
+++ src/constants.js
@@ -0,0 +1,34 @@
+
+import Html from "./assets/icons/html.svg";
+import Css from "./assets/icons/css3.svg";
+import Js from "./assets/icons/javascript.svg";
+import React from "./assets/icons/react.svg";
+import Ts from "./assets/icons/typescript.svg";
+import Gql from "./assets/icons/graphQL.svg";
+
+export const skillsIcons = [
+    {
+      img: Html,
+      title: "HTML5",
+    },
+    {
+      img: Css,
+      title: "CSS3",
+    },
+    {
+      img: Js,
+      title: "Java Script",
+    },
+    {
+      img: Ts,
+      title: "Type Script",
+    },
+    {
+      img: Gql,
+      title: "GraphQL",
+    },
+    {
+      img: React,
+      title: "React",
+    },
+  ];
