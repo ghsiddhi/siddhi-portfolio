5d394c5a9f3d7dcfdddedae0cd403f9b5d7f5ee0
ghsiddhi <siddhiusgupta@gmail.com>
2024-02-09 23:55:49 +0530

    CHORE: format constants

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,3 @@
-
 import Html from "./assets/icons/html.svg";
 import Css from "./assets/icons/css3.svg";
 import Js from "./assets/icons/javascript.svg";
@@ -7,47 +6,47 @@ import Ts from "./assets/icons/typescript.svg";
 import Gql from "./assets/icons/graphQL.svg";
 
 export const skillsIcons = [
-    {
-      img: Html,
-      title: "HTML5",
-    },
-    {
-      img: Css,
-      title: "CSS3",
-    },
-    {
-      img: Js,
-      title: "Java Script",
-    },
-    {
-      img: Ts,
-      title: "Type Script",
-    },
-    {
-      img: Gql,
-      title: "GraphQL",
-    },
-    {
-      img: React,
-      title: "React",
-    },
-  ];
+  {
+    img: Html,
+    title: "HTML5",
+  },
+  {
+    img: Css,
+    title: "CSS3",
+  },
+  {
+    img: Js,
+    title: "Java Script",
+  },
+  {
+    img: Ts,
+    title: "Type Script",
+  },
+  {
+    img: Gql,
+    title: "GraphQL",
+  },
+  {
+    img: React,
+    title: "React",
+  },
+];
 
-  export const navlinks = [
-    {
-      name: "Home",
-      link: "#home",
-    },
-    {
-      name: "About",
-      link: "#about",
-    },
-    {
-      name: "Projects",
-      link: "#projects",
-    },
-    {
-      name: "Contact",
-      link: "#contact",
-    },
-  ];
+export const navlinks = [
+  {
+    name: "Home",
+    link: "#home",
+  },
+  {
+    name: "About",
+    link: "#about",
+  },
+  {
+    name: "Projects",
+    link: "#projects",
+  },
+  {
+    name: "Contact",
+    link: "#contact",
+  },
+];
